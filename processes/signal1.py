31e0bfe648438bf2b6e9ad6db2dce0f2d1eb36bb
anetman <outplanet@163.com>
2017-06-27 15:20:58 +0800

    new process

---
+++ processes/signal1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+import signal
+import time
+
+
+def now():
+    return time.ctime(time.time())
+
+
+def onSignal(signum, stackframe):
+    print('Got signal', signum, 'at', now())
+
+signum = int(sys.argv[1])
+signal.signal(signum, onSignal)
+while True:
+    signal.pause()
